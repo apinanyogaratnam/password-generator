597b06edf1870bbedf224e1557fdc943b45221ef
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-04-23 19:18:01 -0400

    Add files via upload

--- suggest_passwords.c
+++ suggest_passwords.c
@@ -8,32 +8,24 @@
 #define WHITE_SPACE ' '
 #define NUMBER_OF_CONDITIONS 4
 
-void shuffle_password(); // shuffles password to randomize order of different types of conditions
+
 void exists(char *password); // checks if password exists already
-void add_spaces(char *password, int length_of_password, int number_of_spaces); // modifies string with number of spaces given
-char *suggest_password(int length_of_password, bool contains_lowercase, bool contains_uppercase, \
-                       bool contains_special_characters, bool contains_numbers, bool contains_spaces, char *password);
-/*
 
-*/
 
-// given conditions:
-    // contains uppercase
-    // contains lowercase
-    // contains numbers
-    // contains special characters
-    // contains spaces
+long factorial(long n) {
+    if (n == 0 || n == 1) return 1;
+    return n * factorial(n-1);
+}
 
-int factorial(int number) {
-    if (number == 0 || number == 1) return 1;
-    return number * factorial(number-1);
+long permutation(long n, long r) {
+    return pow(n, r);
 }
 
 void delete_password_pointer(char *pointer_to_password) {
     free(pointer_to_password);
 }
 
-void get_lowercase_letters(char *string, int *index_usable, int number_of_randoms) {
+void add_lowercase_letters(char *string, int *index_usable, int number_of_randoms) {
     for (int i=*(index_usable); i<number_of_randoms+(*index_usable); i++) {
         int random_number = rand()%26; // 0-25
         string[i] = random_number+97; // ascii table adding (97-122)
@@ -42,7 +34,7 @@ void get_lowercase_letters(char *string, int *index_usable, int number_of_random
     string[*index_usable] = DELIMITER;
 }
 
-void get_uppercase_letters(char *string, int *index_usable, int number_of_randoms) {
+void add_uppercase_letters(char *string, int *index_usable, int number_of_randoms) {
     for (int i=*(index_usable); i<number_of_randoms+(*index_usable); i++) {
         int random_number = rand()%26; // 0-25
         string[i] = random_number+65; // 65-90
@@ -57,7 +49,7 @@ int get_rand_special_characters_num() {
 
     return ascii_array[random_number];
 }
-void get_special_characters(char *string, int *index_usable, int number_of_randoms) {
+void add_special_characters(char *string, int *index_usable, int number_of_randoms) {
     for (int i=*(index_usable); i<number_of_randoms+(*index_usable); i++) {
         int random_number = get_rand_special_characters_num();
         string[i] = random_number;
@@ -66,7 +58,7 @@ void get_special_characters(char *string, int *index_usable, int number_of_rando
     string[*index_usable] = DELIMITER;
 }
 
-void get_number_characters(char *string, int *index_usable, int number_of_randoms) {
+void add_number_characters(char *string, int *index_usable, int number_of_randoms) {
     for (int i=*(index_usable); i<number_of_randoms+(*index_usable); i++) {
         int random_number = rand()%11;// 0-10
         string[i] = random_number+48; // 48-57
@@ -75,76 +67,139 @@ void get_number_characters(char *string, int *index_usable, int number_of_random
     string[*index_usable] = DELIMITER;
 }
 
-// char *suggest_password(int length_of_password, bool contains_lowercase, bool contains_uppercase, bool contains_special_characters, bool contains_numbers, bool contains_spaces, char *password) {
-//     //33-126 are possible symbols
-//     // as we check the conditions in the parameters, remove the numbers allowed
-//     // need to make a randomizer or import a randomizer library
-//     // loop through length_of_password
-//     // modify up current position of char array
-//     // as of right now passing in char array but need to change to allocate memory for char and free pointer after in another function
-//     char *suggesting_password = (char *)calloc(length_of_password, sizeof(char));
+void add_spaces(char *string, int *index_usable, int number_of_spaces) {
+    for (int i=*(index_usable); i<number_of_spaces+(*index_usable); i++) {
+        string[i] = WHITE_SPACE;
+    }
+    *index_usable += number_of_spaces;
+    string[*index_usable] = DELIMITER;
+}
 
-//     int count = 0;
-//     if (contains_lowercase) count++;
-//     if (contains_uppercase) count++;
-//     if (contains_special_characters) count++;
-//     if (contains_numbers) count++;
-//     if (contains_spaces) count++;
+void shuffle_password(char *string, int length_of_string) {
+    int random_number;
+    char temp;
+    for (int i=0; i<length_of_string; i++) {
+        random_number = rand()%length_of_string;
+        temp = string[i];
+        string[i] = string[random_number];
+        string[random_number] = temp;
+    }
+}
 
-//     int dividend = length_of_password/count;
-//     int remainder = length_of_password%count;
-//     int index_usable = 0;
+char *suggest_password(int length_of_password, bool contains_lowercase, bool contains_uppercase, bool contains_special_characters, bool contains_numbers, bool contains_spaces) {
+    // as we check the conditions in the parameters, remove the numbers allowed
+    // need to make a randomizer or import a randomizer library
+    // loop through length_of_password
+    // modify up current position of char array
+    // as of right now passing in char array but need to change to allocate memory for char and free pointer after in another function
+    if (!contains_lowercase && !contains_uppercase && !contains_special_characters && !contains_numbers && !contains_spaces) {
+        printf("Could not generate password with given input");
+        return NULL;
+    }
+    if (length_of_password <= 0) {
+        printf("Could not generate password with given input");
+        return NULL;
+    }
+    char *suggesting_password = (char *)calloc(length_of_password, sizeof(char));
+    if (!suggesting_password) {
+        printf("No space on ram.\n");
+        return NULL;
+    }
 
-//     if (contains_lowercase) get_lowercase_letters(suggesting_password, &index_usable, dividend);
-//     if (contains_uppercase) get_uppercase_letters(suggesting_password, &index_usable, dividend);
+    int count = 0;
+    if (contains_lowercase) count++;
+    if (contains_uppercase) count++;
+    if (contains_special_characters) count++;
+    if (contains_numbers) count++;
+    if (contains_spaces) count++;
 
-
-//     return "";
-// }
-
-
-
-
-// get_password_strength()
-    //counting number of boolean characters the user chose and length and use length! to count strength
-
-int main() {
-    char name[100];
+    int dividend = length_of_password/count;
+    int remainder = length_of_password%count;
     int index_usable = 0;
 
-    // testing get_lowercase_letters
-    get_lowercase_letters(name, &index_usable, 10);
-    if (strlen(name) != 10) {
-        printf("lowercase letters has an unexpected length of: %lu", strlen(name));
-        return 1;
-    }
-    printf("get_lowercase_letters passed.\n");
+    // fill up string
+    if (contains_lowercase) add_lowercase_letters(suggesting_password, &index_usable, dividend);
+    if (contains_uppercase) add_uppercase_letters(suggesting_password, &index_usable, dividend);
+    if (contains_special_characters) add_special_characters(suggesting_password, &index_usable, dividend);
+    if (contains_numbers) add_number_characters(suggesting_password, &index_usable, dividend);
+    if (contains_spaces) add_spaces(suggesting_password, &index_usable, dividend);
 
-    // testing get_upper_case_letters
-    get_uppercase_letters(name, &index_usable, 10);
-    if (strlen(name) != 20) {
-        printf("uppercase letters has an unexpected length of: %lu\n", strlen(name));
-        return 1;
-    }
-    printf("get_uppercase_letters passed.\n");
+    // early return if no remainders exist
+    if (remainder == 0) return suggesting_password;
 
-    // testing get_special_characters
-    get_special_characters(name, &index_usable, 10);
-    if (strlen(name) != 30) {
-        printf("special characters has an unexpected length of: %lu\n", strlen(name));
-        return 1;
+    // fill up remainder of string
+    if (contains_lowercase) {
+        add_lowercase_letters(suggesting_password, &index_usable, remainder);
+    } else if (contains_uppercase) {
+        add_uppercase_letters(suggesting_password, &index_usable, remainder);
+    } else if (contains_special_characters) {
+        add_special_characters(suggesting_password, &index_usable, remainder);
+    } else if (contains_numbers) {
+        add_number_characters(suggesting_password, &index_usable, remainder);
+    } else {
+        add_spaces(suggesting_password, &index_usable, remainder);
     }
-    printf("get_special_characters passed.\n");
 
-    // testing get_number_characters
-    get_number_characters(name, &index_usable, 10);
-    if (strlen(name) != 40) {
-        printf("number characters has an unexpected length of: %lu\n", strlen(name));
-        return 1;
-    }
-    printf("get_number_characters passed.\n");
-
+    // shuffle password
+    shuffle_password(suggesting_password, length_of_password);
     
-    printf("string: %s\n", name);
-    return 0;
+    return suggesting_password;
+}
+
+
+
+
+long get_password_strength(int length_of_password, bool contains_uppercase, bool contains_lowercase, bool contains_numbers, bool contains_special_characters, bool contains_spaces) {
+    if (length_of_password <= 0) {
+        printf("Cannot determine strength of password with given requirements.\n");
+        return -1;
+    }
+    if (!contains_lowercase && !contains_uppercase && !contains_special_characters && !contains_numbers && !contains_spaces) {
+        printf("Cannot determine strength of password with given requirements.\n");
+        return -1;
+    }
+    int count = 0;
+    if (contains_lowercase) count++;
+    if (contains_uppercase) count++;
+    if (contains_special_characters) count++;
+    if (contains_numbers) count++;
+    if (contains_spaces) count++;
+
+    int dividend = length_of_password/count;
+    int remainder = length_of_password%count;
+
+    long possible_permutaions = 1;
+    if (contains_lowercase) {
+        possible_permutaions *= permutation(26, dividend);
+    }
+    if (contains_uppercase) {
+        possible_permutaions *= permutation(26, dividend);
+    }
+    if (contains_special_characters) {
+        possible_permutaions *= permutation(32, dividend);
+    }
+    if (contains_numbers) {
+        possible_permutaions *= permutation(10, dividend);
+    }
+    if (contains_spaces) {
+        possible_permutaions *= permutation(1, dividend);
+    }
+
+    // early return if no remainder exists
+    if (remainder == 0) return possible_permutaions;
+
+    // check remaining letters
+    if (contains_lowercase) {
+        possible_permutaions *= permutation(26, remainder);
+    } else if (contains_uppercase) {
+        possible_permutaions *= permutation(26, remainder);
+    } else if (contains_special_characters) {
+        possible_permutaions *= permutation(32, remainder);
+    } else if (contains_numbers) {
+        possible_permutaions *= permutation(10, remainder);
+    } else {
+        possible_permutaions *= permutation(1, remainder);
+    }
+
+    return possible_permutaions;
 }
--- suggest_passwords.h
+++ suggest_passwords.h
@@ -1,28 +1,29 @@
 
-int factorial(int number);
+long factorial(long n);
 /*
 
+// get the factorial of n (n!)
 */
 
 void delete_password_pointer(char *pointer_to_password);
 /*
 
+// deletes pointer for suggested password
 */
 
-void get_lowercase_letters(char *string, int index_usable, int number_of_randoms); 
+void add_lowercase_letters(char *string, int index_usable, int number_of_randoms); 
 /*
-
 
 // modifies a string of lowercase letters randomized to use
 */
 
-void get_uppercase_letters(char *string, int *index_usable, int number_of_randoms); 
+void add_uppercase_letters(char *string, int *index_usable, int number_of_randoms); 
 /* 
 
 // modifies a string of uppercase letters randomized to use
 */
 
-void get_special_characters(); 
+void add_special_characters(); 
 /*
 
 // modifies a string of special letters to use
@@ -34,8 +35,48 @@ int get_rand_special_characters_num();
 // get_special_characters helper to get a random special character
 */
 
-void get_number_characters(char *string, int *index_usable, int number_of_randoms); 
+void add_number_characters(char *string, int *index_usable, int number_of_randoms); 
 /*
 
 // modifies a string of numbers to use
+*/
+
+void add_spaces(char *password, int length_of_password, int number_of_spaces); 
+/*
+
+// modifies string with number of spaces given
+*/
+
+void shuffle_password(); 
+/*
+
+// shuffles password to randomize order of different types of conditions
+*/
+
+char *suggest_password(int length_of_password, bool contains_lowercase, bool contains_uppercase, \
+                       bool contains_special_characters, bool contains_numbers, bool contains_spaces);
+/*
+
+// suggest a password given the conditions of the parameters
+
+// given conditions:
+    // contains uppercase
+    // contains lowercase
+    // contains numbers
+    // contains special characters
+    // contains spaces
+    // length_of_string
+*/
+
+long get_password_strength(int length_of_password, bool contains_uppercase, bool contains_lowercase, bool contains_numbers, bool contains_special_characters, bool contains_spaces);
+/*
+
+//counting number of boolean characters the user chose and length and use length! to count strength
+*/
+
+long permutation(long n, long r);
+/*
+
+// Determines number of possible permutations can be applied given n and r
+// P(n, r) = n^r  r = number of spots to be filled, n = possible items to fill the spots
 */
